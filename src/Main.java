62938fd06340eb7d664b08e998680adf993fd997
Nela <nela@gmail.com>
2025-12-09 09:36:25 +0100

    Subida de Archivos de Trabajo

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        System.out.println("1.Gestor de Numeros");
+        System.out.println("2.Conversor");
+        System.out.println("3.Analizador de texto");
+        System.out.println("4.Gestion de Tareas");
+        System.out.println("5.Adivina el numero");
+    }
+}
