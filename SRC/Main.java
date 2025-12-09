91355f43ed6af2e266d0118fddbc1d6010fb0d22
nelfaz <nelfaz@alu.edu.gva.es>
2025-12-09 10:05:56 +0100

    Add files via upload

---
+++ SRC/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner leer = new Scanner (System.in);
+        System.out.println("1.Gestor de Numeros");
+        System.out.println("2.Conversor Multitipo");
+        System.out.println("3.Analixador de tetxto");
+        System.out.println("4.Gestion de Tareas");
+        System.out.println("5.Adivinar el numero");
+
+    }
+}
