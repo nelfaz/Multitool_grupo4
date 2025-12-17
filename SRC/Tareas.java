9ba67d971b97137e67aae7b332f3f6830e7a8df2
Brian <brian.cohen@edu.gva.es>
2025-12-17 13:22:07 +0100

    Hcaiendo el mneu principal

--- SRC/Tareas.java
+++ SRC/Tareas.java
@@ -1,10 +1,39 @@
+import java.util.Scanner;
+
 public class Tareas {
-    static Scanner 
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+
     }
 
     static void menuTareas() {
+        System.out.println("");
+        System.out.println("1.Añadir Tarea");
+        System.out.println("2.Listar Tareas");
+        System.out.println("3.Buscar Palabra");
+        System.out.println("0.Volver");
+        System.out.println("");
+        System.out.println("Que opcion quieres hacer?");
 
+        int seleccion = sc.nextInt();
+
+        switch (seleccion) {
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+
+            case 3:
+
+                break;
+
+            case 0:
+
+                break;
+        }
     }
 }
