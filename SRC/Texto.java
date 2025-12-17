976f3e2309b13fe4f4513e64f22fd6d01a5d1102
Diego-Mayor <diegoflymr@gmail.com>
2025-12-17 14:01:27 +0100

    Estructura inicial del submenú del Analizador de Texto

--- SRC/Texto.java
+++ SRC/Texto.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
+
 public class Texto {
-    
+     public static void iniciar() {
+        /*Opción para contar vocales
+
+        Opción para contar palabras
+
+        Opción para mostrar la palabra más larga
+
+        Opción para volver al menú principal */
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Seleccione una opción digitando el numero: ");
+        System.out.println("1. Contar vocales");
+        System.out.println("2. Contar palabras");
+        System.out.println("3. Mostrar la palabra más larga");
+        System.out.println("4. Volver al menú principal");
+
+        int opción = leer.nextInt();
+        switch (opción) {
+            case 1:
+                contarVocales();
+                break;
+            case 2:
+                contarPalabras();
+                break;
+            case 3:
+                mostrarPalabraMasLarga();
+                break;
+            case 4:
+                return;
+            default:
+                System.out.println("Opción no válida.");
+                break;
+
+            default:
+                break;
+        }
+
+    }
 }
