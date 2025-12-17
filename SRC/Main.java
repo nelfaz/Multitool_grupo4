652352bfce6062ed6895e1691a1cabec85c659cc
Brian <brian.cohen@edu.gva.es>
2025-12-17 15:50:41 +0100

    peuqeñas mejoresd todo el codigo

--- SRC/Main.java
+++ SRC/Main.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
-public class Main{
-    static Scanner leer = new Scanner (System.in);
 
-    //Variables de Gestor de numeros
+public class Main {
+    static Scanner leer = new Scanner(System.in);
+
+    // Variables de Gestor de numeros
     static final int TAM_MAX = 20;
     static int[] numeros = new int[TAM_MAX];
     static int usados = 0;
 
-    //Varable de juego 
+    // Varable de juego
     static int ultimosIntentos = 0;
 
-    //aqui est la main normal
+    // aqui est la main normal
     public static void main(String[] args) {
         SeleccionarOpcion();
 
     }
 
-    //aqui es el switch para interracionar con el menu principal 
-    static void SeleccionarOpcion(){
+    // aqui es el switch para interracionar con el menu principal
+    static void SeleccionarOpcion() {
         System.out.println("");
         System.out.println("Inicio");
         System.out.println("");
@@ -29,41 +30,40 @@ public class Main{
         System.out.println("0.Salir del todo");
         System.out.println("");
         System.out.println("Elige la opcion que desea ejecutar");
-        int seleccion=leer.nextInt();
+        int seleccion = leer.nextInt();
 
-        switch(seleccion){
+        switch (seleccion) {
             case 1:
                 inicio_gestor_de_numeros();
-            break;
+                break;
 
             case 2:
                 iniciar_aplicacion_conversor();
-            break;
+                break;
 
             case 3:
                 System.out.println("Sin funcionamiento el programador no estuvo disponible");
-            break;
+                break;
 
             case 4:
 
-            break;
+                break;
 
             case 5:
                 juegoMain();
-            break;
+                break;
 
             case 0:
                 System.out.println("Saliendo del proograma");
-            break;
+                break;
         }
     }
 
     // metodos ultilzados en cada opcion
 
-    //CONVERSOR 
+    // CONVERSOR
 
-
-    public static void C_to_F(int temp, int tipo){
+    public static void C_to_F(int temp, int tipo) {
         double conv;
         if (tipo == 1) { // C a F
             conv = temp * 9.0 / 5 + 32;
@@ -74,7 +74,7 @@ public class Main{
         }
     }
 
-    public static void C_to_F_UI(){
+    public static void C_to_F_UI() {
         System.out.println("Dime la temperatura: ");
         int temp = leer.nextInt();
         System.out.println("Dime 1 si es Cº o 2 si es Fahrenheit: ");
@@ -83,8 +83,7 @@ public class Main{
         C_to_F(temp, tipo);
     }
 
-
-    public static void M_to_P_UI(){
+    public static void M_to_P_UI() {
         System.out.println("Dime la distancia: ");
         int valor = leer.nextInt();
         System.out.println("Dime 1 si son metros o 2 si son pies: ");
@@ -93,7 +92,7 @@ public class Main{
         M_to_P(valor, tipo);
     }
 
-    public static void M_to_P(int valor, int tipo){
+    public static void M_to_P(int valor, int tipo) {
         double conv;
         if (tipo == 1) {
             conv = valor * 3.28084;
@@ -104,8 +103,7 @@ public class Main{
         }
     }
 
-
-    public static void K_to_L_UI(){
+    public static void K_to_L_UI() {
         System.out.println("Dime el peso: ");
         int valor = leer.nextInt();
         System.out.println("Dime 1 si son kilos o 2 si son libras: ");
@@ -114,7 +112,7 @@ public class Main{
         K_to_L(valor, tipo);
     }
 
-    public static void K_to_L(int valor, int tipo){
+    public static void K_to_L(int valor, int tipo) {
         double conv;
         if (tipo == 1) {
             conv = valor * 2.20462;
@@ -125,8 +123,7 @@ public class Main{
         }
     }
 
-
-    public static int menu(){
+    public static int menu() {
         System.out.println("\n===== MENÚ DE OPCIONES =====");
         System.out.println("1. Celsius ↔ Fahrenheit");
         System.out.println("2. Metros ↔ Pies");
@@ -157,6 +154,7 @@ public class Main{
                     break;
                 case 0:
                     System.out.println("Volviendo a inicio..");
+                    System.out.println("");
                     SeleccionarOpcion();
                     break;
                 default:
@@ -166,8 +164,7 @@ public class Main{
         } while (opcion != 0);
     }
 
-    //GeESTOR DE NUMEROS 
-
+    // GeESTOR DE NUMEROS
 
     static void inicio_gestor_de_numeros() {
         int opcion;
@@ -188,6 +185,8 @@ public class Main{
                     break;
                 case 0:
                     System.out.println("Volviendo a inicio...");
+                    System.out.println("");
+                    SeleccionarOpcion();
                     break;
                 default:
                     System.out.println("Opción inexistente, inténtelo de nuevo");
@@ -195,6 +194,7 @@ public class Main{
             System.out.println();
         } while (opcion != 0);
     }
+
     static int menuGestiondeNúmeros() {
         System.out.println("GESTOR DE NÚMEROS");
         System.out.println("1. Añadir número");
@@ -204,9 +204,10 @@ public class Main{
         System.out.println("0. Volver");
         System.out.print("Elige una opción: ");
         int opcion = leer.nextInt();
-        leer.nextLine(); 
+        leer.nextLine();
         return opcion;
     }
+
     // 1. Añadir número
     static void anadirNumeroUI() {
         if (usados >= TAM_MAX) {
@@ -220,6 +221,7 @@ public class Main{
         usados++;
         System.out.println("Número añadido correctamente.");
     }
+
     // 2. Listar números
     static void listarNumerosUI() {
         if (usados == 0) {
@@ -231,6 +233,7 @@ public class Main{
             System.out.println((i + 1) + ": " + numeros[i]);
         }
     }
+
     // 3. Ver mínimo y máximo
     static void verMinMaxUI() {
         if (usados == 0) {
@@ -250,6 +253,7 @@ public class Main{
         System.out.println("Mínimo: " + min);
         System.out.println("Máximo: " + max);
     }
+
     // 4. Buscar número
     static void buscarNumeroUI() {
         if (usados == 0) {
@@ -271,8 +275,7 @@ public class Main{
         }
     }
 
-
-    //JUEGOS
+    // JUEGOS
     public static void juegoMain() {
         int opcion;
 
@@ -281,59 +284,51 @@ public class Main{
             System.out.println("1. Jugar (número entre 1 y 50)");
             System.out.println("2. Mostrar intentos del último juego");
             System.out.println("0. Volver");
-            
-            if(leer.hasNextInt()){
+
+            if (leer.hasNextInt()) {
                 opcion = leer.nextInt();
-                leer.nextLine(); 
+                leer.nextLine();
             } else {
                 opcion = -1;
-                leer.next(); 
+                leer.next();
             }
 
             if (opcion == 1) {
-                
-                int num , intentos,numJugador;
-                intentos=numJugador=0;
-        
-                num = (int) ((Math.random() * (50))+1);
-        
+
+                int num, intentos, numJugador;
+                intentos = numJugador = 0;
+
+                num = (int) ((Math.random() * (50)) + 1);
+
                 System.out.println("ADIVINA EL NUMERO DEL 1 AL 50");
-                
-        
-        
-                do{
-                    
-                    
-                     System.out.print("INTRODUCE EL NÚMERO: ");
-                    if(leer.hasNextInt()){
-                        
-                       
-                       
-                        numJugador=leer.nextInt();
+
+                do {
+
+                    System.out.print("INTRODUCE EL NÚMERO: ");
+                    if (leer.hasNextInt()) {
+
+                        numJugador = leer.nextInt();
                         leer.nextLine();
-                    
-                    
-                    
-                    if(numJugador!=num){
-                        
-                        System.out.println("INTÉNTALO OTRA VEZ");
-                        System.out.println();
-                    }
-                    
-                    intentos++;}
-                    else{
+
+                        if (numJugador != num) {
+
+                            System.out.println("INTÉNTALO OTRA VEZ");
+                            System.out.println();
+                        }
+
+                        intentos++;
+                    } else {
                         System.out.println("VALOR INTRODUCIDO NO VÁLIDO");
                         leer.next();
-                        }
-        
-                }while(numJugador!=num);
-        
+                    }
+
+                } while (numJugador != num);
+
                 System.out.println();
                 System.out.println("HAS ACERTADO");
                 System.out.println();
-                System.out.println("INTENTOS: "+intentos); 
-                
-                
+                System.out.println("INTENTOS: " + intentos);
+
                 // Guardamos los intentos al terminar la partida
                 ultimosIntentos = intentos;
 
@@ -345,6 +340,10 @@ public class Main{
         } while (opcion != 0);
 
         System.out.println("VOLVIENDO A INICIO");
+        System.out.println("");
+        SeleccionarOpcion();
     }
+
+    // TAREAS
 
 }
